32233400545da013a6e841bf1a5a716322e1695e
samedb <41544886+samedb@users.noreply.github.com>
2019-02-02 16:01:14 +0100

    Zapoceta aplikacija
    Dodat folder Aplikacija i u njemu projekat StudentskaSluzba.
    Projekat ima classLibrary StudentskaSluzba.Model koja koristi Entity Framework za rad sa bazom podataka.
    Ima i main page koji ima prazni navigation view.

---
+++ Aplikacija/Studentska služba/Studentska služba/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using StudentskaSluzba.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Studentska_služba
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async Task PrikaziHelpAsync()
+        {
+            await new MessageDialog("Ovo je help", "Help").ShowAsync();
+        }
+
+        private void MyNavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+
+        }
+
+        private void MyNavigationView_BackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
+        {
+            if (Frame.CanGoBack)
+                Frame.GoBack();
+        }
+
+        private void ContentFrame_NavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async void PrikaziKod_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            await Windows.System.Launcher.LaunchUriAsync(new Uri("https://github.com/samedb/Studentska-sluzba"));
+        }
+
+        private async void PrikaziHelp_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            await new MessageDialog("Ovo je help", "Help").ShowAsync();
+        }
+    }
+}
